04b3ecf3f5bf37c5d4c422800aa7acd6ec3c8678
MirrorXu <xuxulee@163.com>
2020-11-08 23:02:44 +0800

    修改静态png文件的js引入方式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,13 +7,14 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import logoImg from "@/assets/logo.png";
+// import logoImg from "@/assets/logo.png";
 import ColumnList, { ColumnProps } from "@/components/ColumnList.vue"; // @ is an alias to /src
 const testData: ColumnProps[] = new Array(10).fill({
   id: 1,
   title: "test1的专栏",
   description: "test1的专栏-description",
-  avatar: logoImg,
+  // avatar: logoImg,
+  avatar: require("@/assets/logo.png"),
 });
 export default defineComponent({
   name: "Home",
